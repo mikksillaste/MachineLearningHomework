7f278989014aec7d1d6ac13577e8410e69c3a32d
mikksillaste <mikk.sillaste@gmail.com>
2017-12-01 06:56:41 +0200

    iris3 example

--- iris3.py
+++ iris3.py
@@ -1,9 +1,32 @@
+import random
+from scipy.spatial import distance
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
+
 class SkrappyKNN():
     def fit(self, x_train, y_train):
-        pass
+        self.x_train = x_train
+        self.y_train = y_train
 
     def predict(self, x_test):
-        pass
+        predictions = []
+        for row in x_test:
+            label = self.closest(row)
+            predictions.append(label)
+        return predictions
+
+    def closest(self, row):
+        best_dist = euc(row, self.x_train[0])
+        best_index = 0
+        for i in range(1, len(self.x_train)):
+            dist = euc(row, self.x_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
+
 
 from sklearn import datasets
 from sklearn.metrics import accuracy_score
