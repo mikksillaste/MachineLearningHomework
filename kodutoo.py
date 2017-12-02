0683a6d661719f06d37fd9403cc791df24d0facc
mikksillaste <mikk.sillaste@gmail.com>
2017-12-02 20:23:34 +0200

    iris3 example

--- kodutoo.py
+++ kodutoo.py
@@ -3,11 +3,14 @@ def loadAllData(fn):
     all_data = []
     with open(fn) as f:
         next(f)  # esimest rida ei loe
-       # all_data = [[int(s) for s in l.split()] for l in open('andmed.txt').readlines()]
+        # all_data = [[int(s) for s in l.split()] for l in open('andmed.txt').readlines()]
         for line in f:
             all_data.append([line.strip()])
-            # all_data = list(map(int, all_data))
     return all_data
 
 
-print(loadAllData("andmed.txt"))
+test = loadAllData("andmed.txt")[0]
+str1 = ''.join(test)
+mylist = [int(x) for x in str1.split(',')]
+
+print(mylist)
